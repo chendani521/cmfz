e576befa26ad21b73a55981c4d99634d9a3b9665
lichen <18336337067@163.com>
2018-12-19 19:16:23 +0800

    这是我的后期项目

---
+++ src/main/java/com/baizhi/AdminServiceImpl.java
@@ -0,0 +1,18 @@
+package com.baizhi;
+
+import com.baizhi.mapper.AdminMapper;
+import com.baizhi.service.AdminService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+public class AdminServiceImpl implements AdminService {
+    @Autowired
+    AdminMapper adminMapper;
+
+    @Override
+    public void login() {
+    }
+}
